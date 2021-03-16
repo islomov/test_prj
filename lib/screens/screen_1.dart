83226854827cc71d3709c10d584dbd4c20af922a
GoodUser <good.user.777.999@gmail.com>
2021-03-17 00:03:04 +0500

    Scrollbar added

--- lib/screens/screen_1.dart
+++ lib/screens/screen_1.dart
@@ -14,119 +14,122 @@ class Screen1 extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: ProjectColor.grey,
-      child: Center(
-        child: Container(
-          margin: const EdgeInsets.all(ProjectSpacing.spacing20),
-          decoration: const BoxDecoration(
-              color: ProjectColor.white,
-              borderRadius:
-                  BorderRadius.all(Radius.circular(ProjectSpacing.spacing20))),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Container(
-                  margin: const EdgeInsets.fromLTRB(
-                      ProjectSpacing.spacing0,
-                      ProjectSpacing.spacing0,
-                      ProjectSpacing.spacing0,
-                      ProjectSpacing.spacing0),
-                  child: Stack(
-                    children: [
-                      Container(
-                          alignment: Alignment.topRight,
-                          margin: const EdgeInsets.fromLTRB(
-                              ProjectSpacing.spacing0,
-                              ProjectSpacing.spacing5,
-                              ProjectSpacing.spacing10,
-                              ProjectSpacing.spacing0),
-                          child: Material(
-                            color: ProjectColor.white,
+    return Scaffold(
+      body: Container(
+        color: ProjectColor.grey,
+        child: Center(
+          child: Container(
+            margin: const EdgeInsets.all(ProjectSpacing.spacing20),
+            decoration: const BoxDecoration(
+                color: ProjectColor.white,
+                borderRadius:
+                BorderRadius.all(Radius.circular(ProjectSpacing.spacing20))),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+
+                Container(
+                    margin: const EdgeInsets.fromLTRB(
+                        ProjectSpacing.spacing0,
+                        ProjectSpacing.spacing0,
+                        ProjectSpacing.spacing0,
+                        ProjectSpacing.spacing0),
+                    child: Stack(
+                      children: [
+                        Container(
+                            alignment: Alignment.topRight,
+                            margin: const EdgeInsets.fromLTRB(
+                                ProjectSpacing.spacing0,
+                                ProjectSpacing.spacing5,
+                                ProjectSpacing.spacing10,
+                                ProjectSpacing.spacing0),
                             child: IconButton(
                               icon: const Icon(Icons.close),
                               onPressed: () {
                                 Navigator.pop(context);
                               },
-                            ),
-                          )),
-                      Container(
-                        margin: EdgeInsets.fromLTRB(
-                            ProjectSpacing.spacing0,
-                            ProjectSpacing.spacing10,
-                            ProjectSpacing.spacing0,
-                            ProjectSpacing.spacing0),
-                        alignment: Alignment.topCenter,
-                        child: const Text(
-                          ProjectText.screen1_title,
-                          textAlign: TextAlign.center,
-                          style: TextStyle(
-                              color: ProjectColor.black,
-                              fontWeight: FontWeight.bold,
-                              fontSize: ProjectFont.font30),
+                            )),
+                        Container(
+                          margin: EdgeInsets.fromLTRB(
+                              ProjectSpacing.spacing0,
+                              ProjectSpacing.spacing10,
+                              ProjectSpacing.spacing0,
+                              ProjectSpacing.spacing0),
+                          alignment: Alignment.topCenter,
+                          child: const Text(
+                            ProjectText.screen1_title,
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                                color: ProjectColor.black,
+                                fontWeight: FontWeight.bold,
+                                fontSize: ProjectFont.font30),
+                          ),
                         ),
-                      ),
-                    ],
-                  )),
-              Container(
-                margin: const EdgeInsets.fromLTRB(
-                    ProjectSpacing.spacing10,
-                    ProjectSpacing.spacing5,
-                    ProjectSpacing.spacing10,
-                    ProjectSpacing.spacing5),
-                child: const Text(
-                  ProjectText.screen1_subTitle,
-                  textAlign: TextAlign.start,
-                  style: TextStyle(
-                      color: ProjectColor.black,
-                      fontWeight: FontWeight.normal,
-                      fontSize: ProjectFont.font15),
-                ),
-              ),
-              Container(
-                height: ProjectSpacing.spacing400,
-                color: ProjectColor.grey.withOpacity(ProjectSpacing.spacing08),
-                child:Scrollbar(
-                  controller: _scrollController,
-                  isAlwaysShown:true,
-                  child:  SingleChildScrollView(
-                    // ignore: lines_longer_than_80_chars
-                    controller: _scrollController, // <---- Same as the Scrollbar controller
-                    padding: EdgeInsets.all(ProjectSpacing.spacing10),
-                    child: Text(
-                      ProjectText.screen1_desc,
-                      style: TextStyle(color: ProjectColor.black),
-                    ),
-                  ),
-                ),
-              ),
-              Container(
-                margin: const EdgeInsets.fromLTRB(
-                    ProjectSpacing.spacing0,
-                    ProjectSpacing.spacing20,
-                    ProjectSpacing.spacing0,
-                    ProjectSpacing.spacing20),
-                child: RaisedButton(
-                  padding: const EdgeInsets.fromLTRB(
-                      ProjectSpacing.spacing20,
-                      ProjectSpacing.spacing15,
-                      ProjectSpacing.spacing20,
-                      ProjectSpacing.spacing15),
-                  shape: RoundedRectangleBorder(
-                      // ignore: lines_longer_than_80_chars
-                      borderRadius:
-                          BorderRadius.circular(ProjectSpacing.spacing20)),
-                  color: ProjectColor.purple,
-                  onPressed: () {},
+                      ],
+                    )),
+
+                Container(
+                  margin: const EdgeInsets.fromLTRB(
+                      ProjectSpacing.spacing10,
+                      ProjectSpacing.spacing5,
+                      ProjectSpacing.spacing10,
+                      ProjectSpacing.spacing5),
                   child: const Text(
-                    ProjectText.screen1_button_text,
+                    ProjectText.screen1_subTitle,
+                    textAlign: TextAlign.start,
                     style: TextStyle(
-                      color: ProjectColor.white,
+                        color: ProjectColor.black,
+                        fontWeight: FontWeight.normal,
+                        fontSize: ProjectFont.font15),
+                  ),
+                ),
+
+                Container(
+                  height: ProjectSpacing.spacing400,
+                  color: ProjectColor.grey.withOpacity(ProjectSpacing.spacing08),
+                  child:Scrollbar(
+                    controller: _scrollController,
+                    isAlwaysShown:true,
+                    child:  SingleChildScrollView(
+                      // ignore: lines_longer_than_80_chars
+                      controller: _scrollController, // <---- Same as the Scrollbar controller
+                      padding: EdgeInsets.all(ProjectSpacing.spacing10),
+                      child: Text(
+                        ProjectText.screen1_desc,
+                        style: TextStyle(color: ProjectColor.black),
+                      ),
                     ),
                   ),
                 ),
-              ),
-            ],
+
+                Container(
+                  margin: const EdgeInsets.fromLTRB(
+                      ProjectSpacing.spacing0,
+                      ProjectSpacing.spacing20,
+                      ProjectSpacing.spacing0,
+                      ProjectSpacing.spacing20),
+                  child: RaisedButton(
+                    padding: const EdgeInsets.fromLTRB(
+                        ProjectSpacing.spacing20,
+                        ProjectSpacing.spacing15,
+                        ProjectSpacing.spacing20,
+                        ProjectSpacing.spacing15),
+                    shape: RoundedRectangleBorder(
+                      // ignore: lines_longer_than_80_chars
+                        borderRadius:
+                        BorderRadius.circular(ProjectSpacing.spacing20)),
+                    color: ProjectColor.purple,
+                    onPressed: () {},
+                    child: const Text(
+                      ProjectText.screen1_button_text,
+                      style: TextStyle(
+                        color: ProjectColor.white,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
--- lib/screens/screen_2.dart
+++ lib/screens/screen_2.dart
@@ -15,114 +15,122 @@ class Screen2 extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: ProjectColor.white,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Container(
-              alignment: Alignment.topRight,
-              margin: const EdgeInsets.fromLTRB(
-                  ProjectSpacing.spacing0,
-                  ProjectSpacing.spacing45,
-                  ProjectSpacing.spacing0,
-                  ProjectSpacing.spacing0),
-              child: Material(
-                color: ProjectColor.white,
+    return Scaffold(
+      body: Container(
+        color: ProjectColor.white,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Container(
+                alignment: Alignment.topRight,
+                margin: const EdgeInsets.fromLTRB(
+                    ProjectSpacing.spacing0,
+                    ProjectSpacing.spacing45,
+                    ProjectSpacing.spacing0,
+                    ProjectSpacing.spacing0),
                 child: IconButton(
                   icon: const Icon(Icons.close),
                   onPressed: () {
                     Navigator.pop(context);
                   },
-                ),
-              )),
-          Container(
-            margin: const EdgeInsets.all(ProjectSpacing.spacing10),
-            child: const Text(
-              ProjectText.screen2_section_1,
-              textAlign: TextAlign.start,
-              style: TextStyle(
-                  color: ProjectColor.black,
-                  fontWeight: FontWeight.bold,
-                  fontSize: ProjectFont.font20),
+                )),
+            Container(
+              margin: const EdgeInsets.all(ProjectSpacing.spacing10),
+              child: const Text(
+                ProjectText.screen2_section_1,
+                textAlign: TextAlign.start,
+                style: TextStyle(
+                    color: ProjectColor.black,
+                    fontWeight: FontWeight.bold,
+                    fontSize: ProjectFont.font20),
+              ),
             ),
-          ),
-          Container(
-            height: ProjectSpacing.spacing120,
-            margin: const EdgeInsets.fromLTRB(
-                ProjectSpacing.spacing26,
-                ProjectSpacing.spacing0,
-                ProjectSpacing.spacing0,
-                ProjectSpacing.spacing0),
-            child: ListView.builder(
-                scrollDirection: Axis.horizontal,
-                itemCount: 2,
-                itemBuilder: (BuildContext context, index) {
-                  var provider = new HorizontalItemsProvider();
-                  return Container(
-                    width: MediaQuery.of(context).size.width - 72,
-                    height: ProjectSpacing.spacing120,
-                    margin: const EdgeInsets.fromLTRB(
-                        ProjectSpacing.spacing5,
-                        ProjectSpacing.spacing0,
-                        ProjectSpacing.spacing5,
-                        ProjectSpacing.spacing0),
-                    child: Card(
-                      elevation: 5,
-                      child: Center(
-                        child: Text(
-                          provider.generateItemAt(index + 1),
-                          style: const TextStyle(
-                              color: ProjectColor.black,
-                              fontWeight: FontWeight.bold,
-                              fontSize: ProjectFont.font20),
-                        ),
-                      ),
-                    ),
-                  );
-                }),
-          ),
-          Container(
-            margin: const EdgeInsets.all(ProjectSpacing.spacing10),
-            child: const Text(
-              ProjectText.screen2_section_2,
-              textAlign: TextAlign.start,
-              style: TextStyle(
-                  color: ProjectColor.black,
-                  fontWeight: FontWeight.bold,
-                  fontSize: ProjectFont.font20),
+            _buildHorizontalList(),
+            Container(
+              margin: const EdgeInsets.all(ProjectSpacing.spacing10),
+              child: const Text(
+                ProjectText.screen2_section_2,
+                textAlign: TextAlign.start,
+                style: TextStyle(
+                    color: ProjectColor.black,
+                    fontWeight: FontWeight.bold,
+                    fontSize: ProjectFont.font20),
+              ),
             ),
-          ),
-          Expanded(
-            child: Container(
-              child: ListView.builder(
-                  padding: const EdgeInsets.all(ProjectSpacing.spacing8),
-                  itemCount: 4,
-                  itemBuilder: (BuildContext context, int index) {
-                    var provider = new VerticalItemsProvider();
-                    return Container(
-                      height: ProjectSpacing.spacing120,
-                      margin: const EdgeInsets.fromLTRB(
-                          ProjectSpacing.spacing18,
-                          ProjectSpacing.spacing5,
-                          ProjectSpacing.spacing18,
-                          ProjectSpacing.spacing5),
-                      child: Card(
-                        elevation: 5,
-                        child: Center(
-                          child: Text(provider.generateItemAt(index + 1),
-                              style: const TextStyle(
-                                  color: ProjectColor.black,
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: ProjectFont.font20)),
-                        ),
-                      ),
-                    );
-                  }),
-            ),
-          )
-        ],
+            Expanded(
+              child: _buildVerticalList(),
+            )
+          ],
+        ),
       ),
     );
   }
+
+  Widget _buildHorizontalList(){
+    return Container(
+      height: ProjectSpacing.spacing120,
+      margin: const EdgeInsets.fromLTRB(
+          ProjectSpacing.spacing26,
+          ProjectSpacing.spacing0,
+          ProjectSpacing.spacing0,
+          ProjectSpacing.spacing0),
+      child: ListView.builder(
+          scrollDirection: Axis.horizontal,
+          itemCount: 2,
+          itemBuilder: (BuildContext context, index) {
+            var provider = new HorizontalItemsProvider();
+            return Container(
+              width: MediaQuery.of(context).size.width - 72,
+              height: ProjectSpacing.spacing120,
+              margin: const EdgeInsets.fromLTRB(
+                  ProjectSpacing.spacing5,
+                  ProjectSpacing.spacing0,
+                  ProjectSpacing.spacing5,
+                  ProjectSpacing.spacing0),
+              child: Card(
+                elevation: 5,
+                child: Center(
+                  child: Text(
+                    provider.generateItemAt(index + 1),
+                    style: const TextStyle(
+                        color: ProjectColor.black,
+                        fontWeight: FontWeight.bold,
+                        fontSize: ProjectFont.font20),
+                  ),
+                ),
+              ),
+            );
+          }),
+    );
+  }
+
+  Widget _buildVerticalList(){
+    return Container(
+      child: ListView.builder(
+          padding: const EdgeInsets.all(ProjectSpacing.spacing8),
+          itemCount: 4,
+          itemBuilder: (BuildContext context, int index) {
+            var provider = new VerticalItemsProvider();
+            return Container(
+              height: ProjectSpacing.spacing120,
+              margin: const EdgeInsets.fromLTRB(
+                  ProjectSpacing.spacing18,
+                  ProjectSpacing.spacing5,
+                  ProjectSpacing.spacing18,
+                  ProjectSpacing.spacing5),
+              child: Card(
+                elevation: 5,
+                child: Center(
+                  child: Text(provider.generateItemAt(index + 1),
+                      style: const TextStyle(
+                          color: ProjectColor.black,
+                          fontWeight: FontWeight.bold,
+                          fontSize: ProjectFont.font20)),
+                ),
+              ),
+            );
+          }),
+    );
+  }
+
 }
